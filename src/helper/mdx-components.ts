e59e7a28357fe141f0c55bb40f75116cdb35d678
Sam Lam <xlin67948gmail.com>
2026-02-02 11:17:37 +0800

    refactor: delete template article, add album images, upgrade React Motion

--- src/helper/mdx-components.ts
+++ src/helper/mdx-components.ts
@@ -1,9 +1,35 @@
 import CodeSnippet from "@/utils/codeSnippet";
 import PasswordToggle from "@/app/_components/inputTest";
+import ResponsiveTextAnimation from "@/app/_components/responsiveTextAnimation"
+import TransformDrag from "@/app/_components/transformDrag";
+import AnimatingRemoved from "@/app/_components/animatingRemoved";
+import Variants from "@/app/_components/variants";
+import StaggeredAnimations from "@/app/_components/staggeredAnimations";
+import ResponsiveDrawer from "@/app/_components/responsiveDrawer";
+import WhileInView from "@/app/_components/whileInView";
+import UseInView from "@/app/_components/useInView";
+import UseScroll from "@/app/_components/useScroll";
+import ScrollProgress from "@/app/_components/scrollProgress";
+import LayoutAnimation from "@/app/_components/layoutAnimation";
+import LayoutId from "@/app/_components/layoutId";
+import GridLayout from "@/app/_components/gridLayout";
 
 const COMPONENT_MAP = {
     pre: CodeSnippet,
     PasswordToggle,
+    ResponsiveTextAnimation,
+    TransformDrag,
+    AnimatingRemoved,
+    Variants,
+    StaggeredAnimations,
+    ResponsiveDrawer,
+    WhileInView,
+    UseInView,
+    UseScroll,
+    ScrollProgress,
+    LayoutAnimation,
+    LayoutId,
+    GridLayout,
 }
 
 export default COMPONENT_MAP;
